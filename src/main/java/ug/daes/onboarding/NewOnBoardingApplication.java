35b7ba46c699b3e94125d468c12dd32860cf2455
deepthi.ponnaganti <sai.deepthi@iamdigitaltrust.com>
2026-02-20 16:44:27 +0530

    Added Rest Template in Main Application

--- src/main/java/ug/daes/onboarding/NewOnBoardingApplication.java
+++ src/main/java/ug/daes/onboarding/NewOnBoardingApplication.java
@@ -3,9 +3,14 @@ package ug.daes.onboarding;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.util.TimeValue;
 import org.apache.hc.core5.util.Timeout;
-
+import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -34,8 +39,20 @@ public class NewOnBoardingApplication {
 				.setResponseTimeout(Timeout.ofSeconds(30))
 				.build();
 
+		SSLConnectionSocketFactory sslSocketFactory =
+				SSLConnectionSocketFactoryBuilder.create()
+						.setSslContext(SSLContexts.createSystemDefault())
+						.setHostnameVerifier(new DefaultHostnameVerifier())
+						.build();
+
+		PoolingHttpClientConnectionManager connectionManager =
+				PoolingHttpClientConnectionManagerBuilder.create()
+						.setSSLSocketFactory(sslSocketFactory)
+						.build();
+
 		CloseableHttpClient httpClient = HttpClients.custom()
 				.setDefaultRequestConfig(requestConfig)
+				.setConnectionManager(connectionManager)
 				.evictExpiredConnections()
 				.evictIdleConnections(TimeValue.ofMinutes(1))
 				.build();
